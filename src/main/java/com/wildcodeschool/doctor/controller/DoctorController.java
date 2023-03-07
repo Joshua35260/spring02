ac9cd7800b92a64830438a58c09fbe6987c2e260
Joshua Dupin <joshua35260@gmail.com>
2023-03-07 11:26:55 +0100

    refactor

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,28 +1,25 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 public class DoctorController {
 
     @GetMapping("/doctor/{number}")
     @ResponseBody
-    public ResponseEntity<?> doctor(@PathVariable int number) {
+    public Doctor getDoctor(@PathVariable int number) {
         if (number == 13) {
-            Doctor doctor = new Doctor(13, "Jodie Whittaker");
-            return ResponseEntity.ok(doctor);
+            return new Doctor(13, "Jodie Whittaker");
         } else if (number >= 1 && number <= 12) {
-            return ResponseEntity.status(HttpStatus.SEE_OTHER).build();
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "Doctor not found");
         } else {
-            String message = "Impossible de récupérer l'incarnation " + number;
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number);
         }
     }
 }
