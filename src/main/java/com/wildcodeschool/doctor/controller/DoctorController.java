d8f9b7d2bc78c4164c5a7c83237c4757e53d456d
Joshua Dupin <joshua35260@gmail.com>
2023-03-07 11:16:39 +0100

    ok

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,17 +1,28 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/")
+    @GetMapping("/doctor/{number}")
     @ResponseBody
-    public Doctor doctor() {
-
-        return null;
+    public ResponseEntity<?> doctor(@PathVariable int number) {
+        if (number == 13) {
+            Doctor doctor = new Doctor(13, "Jodie Whittaker");
+            return ResponseEntity.ok(doctor);
+        } else if (number >= 1 && number <= 12) {
+            return ResponseEntity.status(HttpStatus.SEE_OTHER).build();
+        } else {
+            String message = "Impossible de récupérer l'incarnation " + number;
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(message);
+        }
     }
 }
